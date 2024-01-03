e47fa39c44f53ee646e2fecee14b91f959f3d93c
flawmop <flawmop@insilicosoft.com>
2024-01-02 19:14:48 -0800

    Add minimal controller

---
+++ src/main/java/com/insilicosoft/portal/svc/welcome/WelcomeSvcApplication.java
@@ -0,0 +1,13 @@
+package com.insilicosoft.portal.svc.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insilicosoft/portal/svc/welcome/WelcomeSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insilicosoft.portal.svc.welcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
