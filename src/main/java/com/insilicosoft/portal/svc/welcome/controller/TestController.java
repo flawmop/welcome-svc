296f325eb35620d1d55a7a8c5b619b0a81c1200a
flawmop <flawmop@insilicosoft.com>
2024-02-08 21:40:59 -0800

    Controller mapping consistency

--- src/main/java/com/insilicosoft/portal/svc/welcome/controller/TestController.java
+++ src/main/java/com/insilicosoft/portal/svc/welcome/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
+@RequestMapping("/test")
 public class TestController {
 
   private final PortalProperties portalProperties;
